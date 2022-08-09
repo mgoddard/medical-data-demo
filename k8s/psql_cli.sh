50901d11a4c8ff1c2d26abdf468d5880059c2cce
mgoddard <mgoddard@cockroachlabs.com>
2022-08-09 19:41:13 -0400

    Updating the psql CLI wrapper

--- k8s/psql_cli.sh
+++ k8s/psql_cli.sh
@@ -5,5 +5,5 @@ lb_ip=$( kubectl describe service crdb-lb | perl -ne 'chomp; print "$1\n" if /^L
 # Get the CA cert if necessary
 [ -f "/tmp/ca.crt" ] || ./get_ca_cert.sh
 
-psql "postgresql://tourist:tourist@${lb_ip}:26257/defaultdb?sslmode=require&sslrootcert=/tmp/ca.crt"
+psql "postgresql://demouser:demopasswd@${lb_ip}:26257/defaultdb?sslmode=require&sslrootcert=/tmp/ca.crt"
 
